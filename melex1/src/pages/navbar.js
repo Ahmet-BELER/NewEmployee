fc49d7b7c0e75a49bea30a002fd9251cc07c2cc2
Ahmet <ahmetbeler1739@gmail.com>
2022-06-14 22:59:43 +0300

    home

---
+++ melex1/src/pages/navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+const Menü = styled.section`
+top: 0px;
+left: 0px;
+width: 1920px;
+height: 212px;
+
+background: #FFFFFF 0% 0% no-repeat padding-box;
+opacity: 1;
+  
+`;
+
+
+export default function Navbar() {
+  return (
+   
+<Menü>
+
+
+
+</Menü>
+          
+ 
+  )
+}
